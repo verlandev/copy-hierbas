ae72d529b8400b070e218b45d34620e416aa00a0
Bemart93 <bemart93@gmail.com>
2025-01-31 16:56:57 +0100

    ✨ Incluimos tipografía

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade-in": "fadeIn 2s ease-out forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+    },
   },
   plugins: []
 }
