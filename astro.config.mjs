3355d668e6549a929062302eeca676e740626f80
Bemart93 <bemart93@gmail.com>
2025-01-31 21:38:44 +0100

    📱 Adaptamos la sección Hero para visualizar en dispositivos móviles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind"; // Importa el plugin de Tailwind
 
 export default defineConfig({
+  experimental: {
+    svg: {
+      mode: 'sprite'
+    },
+  },	
   integrations: [tailwind()], // Activa Tailwind en Astro
 });
