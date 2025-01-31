816b05cef4c6e2d72c22fa50a63e7633fe40505f
Bemart93 <bemart93@gmail.com>
2025-01-31 16:36:22 +0100

    ⬆️ Fix: instalación de Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind"; // Importa el plugin de Tailwind
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [tailwind()], // Activa Tailwind en Astro
 });
