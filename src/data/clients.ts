a4b473f3916534ddb1e366117905ccde78f8d663
Bemart93 <bemart93@gmail.com>
2025-02-02 10:33:36 +0100

    ✨ Creamos sección de autoridad

---
+++ src/data/clients.ts
@@ -0,0 +1,57 @@
+import type { Client } from '../types/Client';
+
+export const clients: Client[] = [
+  {
+    name: 'Soy Copywriter',
+    logo: '/logos/soy-copywriter.svg',
+    isSVG: true,
+    isWhite: true,
+    width: 130    
+  },
+  {
+    name: 'Alex Rovira',
+    logo: '/logos/AlexRovira.png',
+    isSVG: false,
+    isWhite: true,
+    width: 130,
+    height: 52
+  },
+  {
+    name: 'Victor Martín',
+    logo: '/logos/victor-martin.png',
+    isSVG: false,
+    isWhite: true,
+    width: 130, 
+    height: 44,
+  },
+  {
+    name: 'Incubadora de Lanzamientos',
+    logo: '/logos/incubadora-lanzamientos.webp',
+    isSVG: false,
+    isWhite: false,
+    width: 130, 
+    height: 100
+  },
+  {
+    name: 'Mariana Fresnedo',
+    logo: '/logos/mariana-fresnedo.webp',
+    isSVG: false,
+    isWhite: false,
+    width: 130, 
+    height: 200
+  },
+  {
+    name: 'Quantum Quip',
+    logo: '/logos/quantum-quip.svg',
+    isSVG: true,
+    isWhite: true,
+    width: 130,
+  },
+  {
+    name: 'Big Bang Conversion',
+    logo: '/logos/bigbangConversion.svg',
+    isSVG: true,
+    isWhite: true,
+    width: 130    
+  }
+]
