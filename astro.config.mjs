c7717248ed1c3d3c8550bb3031dc34cfc5e5d424
Bemart93 <bemart93@gmail.com>
2025-02-02 19:12:56 +0100

    🔧 Setup: inhabilitamos la devtoolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind"; // Importa el plugin de Tailwind
 
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   experimental: {
     svg: {
       mode: 'sprite'
